4d2d061d8003ef103617e434c032b0f37b541b40
Samir <Samirshahub@gmail.com>
2021-12-11 02:58:05 +0200

    SS-3 feat: create 2 nodes

---
+++ main.tf
@@ -0,0 +1,84 @@
+locals {
+  timestamp = "${timestamp()}"
+  timestamp_no_hyphens = "${replace("${local.timestamp}", "-", "")}"
+  timestamp_no_spaces = "${replace("${local.timestamp_no_hyphens}", " ", "")}"
+  timestamp_no_t = "${replace("${local.timestamp_no_spaces}", "T", "")}"
+  timestamp_no_z = "${replace("${local.timestamp_no_t}", "Z", "")}"
+  timestamp_no_colons = "${replace("${local.timestamp_no_z}", ":", "")}"
+  timestamp_sanitized = "${local.timestamp_no_colons}"
+}
+
+provider "google" {
+  credentials = var.credentials
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+  user_project_override = true
+}
+
+resource "google_compute_instance" "Node_1" {
+  name = "Node-${local.timestamp_sanitized}"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20201014"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+}
+
+resource "google_compute_instance" "Node_2" {
+  name = "Node-${local.timestamp_sanitized}"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20201014"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+}
+
+resource "google_compute_firewall" "my_firewall" {
+  name = "default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = 22
+  }
+}
+
+data "template_file" "dev_hosts" {
+  template = file("${path.module}/templates/dev_hosts.cfg")
+  depends_on = [
+    google_compute_instance.Node_1,
+    google_compute_instance.Node_2,
+  ]
+  vars = {
+    api_node_1 = google_compute_instance.Node_1.network_interface.0.access_config.0.nat_ip
+    api_node_2 = google_compute_instance.Node_2.network_interface.0.access_config.0.nat_ip
+  }
+}
+
+resource "null_resource" "dev-hosts" {
+  triggers = {
+    template_rendered = data.template_file.dev_hosts.rendered
+  }
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.dev_hosts.rendered}' > inventory.txt"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  default = "project-2-297319"
+}
+variable "credentials" {
+  default = "project-2-297319-918b48a4ad3f.json"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "user" {
+  default = "samir"
+}
